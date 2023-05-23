a5472068e0d0b9faf1d5d0abae5d98955847f5f6
AlexBolot <a.bolot@sprintechnology.com>
2023-05-23 15:55:51 +0200

    initial commit

---
+++ bin/main.dart
@@ -0,0 +1,5 @@
+import 'package:project_class_analysis/project_class_analysis.dart' as project_class_analysis;
+
+void main(List<String> arguments) {
+  print('Hello world: ${project_class_analysis.calculate()}!');
+}
---
+++ test/project_class_analysis_test.dart
@@ -0,0 +1,8 @@
+import 'package:project_class_analysis/project_class_analysis.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
